588fb4db776e252f4c21a9d5284513c4c28a24ec
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-09-30 12:04:54 +0530

    creating reducers

---
+++ src/app/counter/state/counter.reducer.ts
@@ -0,0 +1,29 @@
+import { createReducer, on } from "@ngrx/store"
+import { initialCounterState } from "./counter.state"
+import { decrement, increment, reset } from "./counter.actions"
+import { stat } from "fs"
+
+const _counterReducer = createReducer(initialCounterState,
+  on(increment, state=>{
+    return {
+      ...state,
+      count : state.count + 1
+    }
+  }),
+  on(decrement, state=>{
+    return {
+      ...state,
+      count : state.count - 1
+    }
+  }),
+  on(reset,state=>{
+    return {
+      ...state,
+      count : 0
+    }
+  })
+)
+
+export function counterReducer(state : any, action:any){
+  return _counterReducer(state, action)
+}
